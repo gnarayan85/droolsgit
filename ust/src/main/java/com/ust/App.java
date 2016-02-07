dd9e3d2bbfc7635fa3b8c55d6fc9eb1c656fd240
admin <gopala498@gmail.com>
2016-02-07 17:30:01 +0530

    commit

--- ust/src/main/java/com/ust/App.java
+++ ust/src/main/java/com/ust/App.java
@@ -12,7 +12,7 @@ public class App  implements java.io.Serializable
 	 */
 	private static final long serialVersionUID = 3152413949083861783L;
 	@org.kie.api.definition.type.Label(value = "totalCost")
-private int totalCost;
+private int totalCost; 
 
 public int getTotalCost() {
 	return totalCost;
